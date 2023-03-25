87a154c8f5554c0f4952aa08e4db55c64e999663
Mayank Sharma <e21cseu0886@bennett.edu.in>
2023-03-25 18:24:50 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+[PPT](https://docs.google.com/presentation/d/17aM8u_8mOAW0ugk8y3lK6YFJjTsuRcA7/edit?usp=sharing&ouid=109528234710057275954&rtpof=true&sd=true)
---
+++ exploreAR/js/monuments.js
@@ -0,0 +1,110 @@
+var list = {
+    animals: [
+        {
+            title: "Eiffel Tower",
+            link: "https://console.echoar.xyz/webar?key=noisy-bar-1396&entry=1bbbec3d-f398-45b8-a353-e46b566f5d90",
+            left: 10,
+            longlat: [48.85, 2.29],
+            foundin: ["Paris, France"],
+            info: "- The Eiffel Tower is a wrought-iron lattice tower on the Champ de Mars in Paris, France. It is named after the engineer Gustave Eiffel, whose company designed and built the tower. - https://en.wikipedia.org/wiki/Eiffel_Tower",
+        },
+        {
+            title: "Taj Mahal",
+            link: "https://console.echoar.xyz/webar?key=cool-wind-0510&entry=41276508-fef2-41c4-b298-07bc678cef07",
+            left: 50000,
+            longlat: [27.17, 78.04],
+            foundin: ["Agra, India"],
+            info: "The Taj Mahal is an ivory-white marble mausoleum on the southern bank of the river Yamuna in the Indian city of Agra. It was commissioned in 1632 by the Mughal emperor Shah Jahan to house the tomb of his favourite wife, Mumtaz Mahal; it also houses the tomb of Shah Jahan himself.- https://en.wikipedia.org/wiki/Taj_Mahal",
+        },
+        {
+            title: "Statue of Liberty",
+            link: "https://console.echoar.xyz/webar?key=cool-wind-0510&entry=003f2670-a5c1-48b8-96a9-7eb5f1552885",
+            left: 6590,
+            longlat: [40.68, -74.04],
+            foundin: ["New York, USA"],
+            info: "The Statue of Liberty is a colossal neoclassical sculpture on Liberty Island in New York Harbor within New York City, in the United States. - https://en.wikipedia.org/wiki/Statue_of_Liberty",
+        },
+        {
+            title: "Machu Picchu",
+            link: "https://console.echoar.xyz/webar?key=cool-wind-0510&entry=b20babe4-d267-47bc-9379-5ae480daa596",
+            left: 1063,
+            longlat: [-13.16, -72.54],
+            foundin: ["Peru"
+          ],
+            info: "Machu Picchu is an Incan citadel set high in the Andes Mountains in Peru, above the Urubamba River valley. Built in the 15th century and later abandoned, it’s renowned for its sophisticated dry-stone walls that fuse huge blocks without the use of mortar, intriguing buildings that play on astronomical alignments and panoramic views. - https://en.wikipedia.org/wiki/Machu_Picchu",
+        },
+        {
+            title: "Colosseum",
+            link: "https://console.echoar.xyz/webar?key=fancy-dust-5994&entry=8e22bf5f-dfb2-49b3-b022-38379e751d32",
+            left: 68,
+            longlat: [41.89, 12.09],
+            foundin: ["Italy, Rome"],
+            info: "The Colosseum, is an oval amphitheatre in the centre of the city of Rome, Italy, just east of the Roman Forum and is the largest ancient amphitheatre ever built, and is still the largest standing amphitheater in the world today, despite its age. - https://en.wikipedia.org/wiki/Colosseum",
+        },
+        {
+            title: "Petra, Jordan",
+            link: "https://console.echoar.xyz/webar?key=fancy-dust-5994&entry=af3db530-f012-4a34-9bc1-3fd99f221b70",
+            left: 100,
+            longlat: [30.32, 35.44],
+            foundin: ["Jordan"],
+            info: "Petra is a famous archaeological site in Jordan's southwestern desert. Dating to around 300 B.C., it was the capital of the Nabatean Kingdom. Accessed via a narrow canyon called Al Siq, it contains tombs and temples carved into pink sandstone cliffs, earning its nickname, the Rose City. -  https://en.wikipedia.org/wiki/Petra",
+        },
+        {
+            title: "Golden Gate Bridge",
+            link: "https://console.echoar.xyz/webar?key=cool-wind-0510&entry=71dc00f7-93fe-4c8b-b378-d7e2866ad63b",
+            left: 1864,
+            longlat: [37.81, -122.47],
+            foundin: ["South-West, China"],
+            info: "The Golden Gate Bridge is a suspension bridge spanning the Golden Gate, the one-mile-wide strait connecting San Francisco Bay and the Pacific Ocean. - https://en.wikipedia.org/wiki/Golden_Gate_Bridge",
+        },
+        {
+            title: "Great Wall of China",
+            link: "https://console.echoar.xyz/webar?key=cool-wind-0510&entry=b2e185bd-3897-4b1f-804e-1b300817ca57",
+            left: 3900,
+            longlat: [40.43, 116.57],
+            foundin: [
+                "China"
+            ],
+            info: "The Great Wall of China is a series of fortifications that were built across the historical northern borders of ancient Chinese states and Imperial China as protection against various nomadic groups from the Eurasian Steppe. - https://en.wikipedia.org/wiki/Great_Wall_of_China",
+        },
+        {
+            title: "Uluru",
+            link: "https://console.echoar.xyz/webar?key=cool-wind-0510&entry=880be458-b1a0-4ba2-99c2-93d5e18b7131",
+            left: 505,
+            longlat: [-25.34, 131.03],
+            foundin: ["Australia"],
+            info: "Uluru, or Ayers Rock, is a massive sandstone monolith in the heart of the Northern Territory’s arid Red Centre. The nearest large town is Alice Springs, 450km away. Uluru is sacred to indigenous Australians and is thought to have started forming around 550 million years ago. - https://en.wikipedia.org/wiki/Uluru",
+        },
+        {
+            title: "Christ the Redeemer",
+            link: "https://console.echoar.xyz/webar?key=cool-wind-0510&entry=9be08598-8a24-4e22-a80a-422a11bb698b",
+            left: 24000,
+            longlat: [-22.95, -43.21],
+            foundin: [
+            "Rio de Janeiro, Brazil"
+            ],
+            info: "Christ the Redeemer is an Art Deco statue of Jesus Christ in Rio de Janeiro, Brazil, created by French sculptor Paul Landowski and built by Brazilian engineer Heitor da Silva Costa, in collaboration with French engineer Albert Caquot. Romanian sculptor Gheorghe Leonida fashioned the face. - https://en.wikipedia.org/wiki/Christ_the_Redeemer_(statue)",
+        },
+        {
+            title: "Sydney Opera House",
+            link: "https://console.echoar.xyz/webar?key=cool-wind-0510&entry=d94c7310-3cec-40f9-8919-86424dde93a0",
+            left: 2400,
+            longlat: [-33.85, 151.21],
+            foundin: [
+                "Sydney, Australia"
+            ],
+            info: "The Sydney Opera House is a multi-venue performing arts centre at Sydney Harbour located in Sydney, New South Wales, Australia. It is one of the 20th century's most famous and distinctive buildings. - https://en.wikipedia.org/wiki/Sydney_Opera_House",
+        },
+        {
+            title: "Spasskaya Bashnya, Moscow",
+            link: "https://console.echoar.xyz/webar?key=cool-wind-0510&entry=e15040ff-9c6c-4d73-8f60-276f8a48f86f",
+            left: 2400,
+            longlat: [55.75, 37.62],
+            foundin: [
+               "Russia"
+            ],
+            info: "The Spasskaya Tower, translated as 'Saviour Tower', is the main tower on the eastern wall of the Moscow Kremlin which overlooks Red Square. - https://en.wikipedia.org/wiki/Spasskaya_Tower",
+        },
+    ],
+
+};
